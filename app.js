1917958c965c9fb0f1431b549d3a41b0e806b303
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2024-12-21 14:34:47 +0100

    feat: psot messages to the backend and updating the UI

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   let sendButton = document.querySelector("#send");
+  let nameInput = document.querySelector("#name");
+  let messageInput = document.querySelector("#message");
+
   sendButton.addEventListener("click", () => {
-    addMessages({ name: "billy Jekins", message: "Whats up my boy" });
+    let message = {
+      name: nameInput.value,
+      message: messageInput.value,
+    };
+    postMessages(message);
   });
   getMessages();
 });
@@ -18,4 +25,22 @@ function getMessages() {
   response
     .then((data) => data.json())
     .then((data) => data.forEach(addMessages));
+}
+function postMessages(message) {
+  let response = fetch("http://localhost:3000/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+  response
+    .then((response) => {
+      if (!response) {
+        throw new Error("HTTP error, status: " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 }
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
 app.use(express.static(__dirname));
+// parsing the request body to be uderstood by node
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 let messages = [
   { name: "Jonny Bravo", message: "I look so sexy" },
   { name: "Tom Cat", message: "Is you is, or is you ain`t my baby" },
@@ -8,6 +13,12 @@ let messages = [
 
 app.get("/messages", (request, response) => {
   response.send(messages);
+});
+
+app.post("/messages", (request, response) => {
+  console.log(request.body);
+  messages.push(request.body);
+  response.sendStatus(200);
 });
 
 let server = app.listen(3000, () => {
